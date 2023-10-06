a0df7044434eb6e42263b06a799c61d97f28c384
cnrprod-team-50452 <1690610961@codenrock.com>
2023-10-06 22:45:11 +0600

    good

--- hibanana.py
+++ hibanana.py
@@ -10,7 +10,6 @@ WALL_TILE_X = 4
 
 scroll_x = 0
 player = None
-enemies = []
 
 
 def get_tile(tile_x, tile_y):
@@ -65,21 +64,6 @@ def is_wall(x, y):
     tile = get_tile(x // 8, y // 8)
     return tile == TILE_FLOOR or tile[0] >= WALL_TILE_X
 
-
-def spawn_enemy(left_x, right_x):
-    left_x = pyxel.ceil(left_x / 8)
-    right_x = pyxel.floor(right_x / 8)
-    for x in range(left_x, right_x + 1):
-        for y in range(16):
-            tile = get_tile(x, y)
-            if tile == TILE_SPAWN1:
-                enemies.append(Enemy1(x * 8, y * 8))
-            elif tile == TILE_SPAWN2:
-                enemies.append(Enemy2(x * 8, y * 8))
-            elif tile == TILE_SPAWN3:
-                enemies.append(Enemy3(x * 8, y * 8))
-
-
 def cleanup_list(list):
     i = 0
     while i < len(list):
@@ -123,7 +107,6 @@ class Player:
         if self.x > scroll_x + SCROLL_BORDER_X:
             last_scroll_x = scroll_x
             scroll_x = min(self.x - SCROLL_BORDER_X, 240 * 8)
-            spawn_enemy(last_scroll_x + 128, scroll_x + 127)
         if self.y >= pyxel.height:
             game_over()
 
@@ -132,111 +115,13 @@ class Player:
         w = 8 if self.direction > 0 else -8
         pyxel.blt(self.x, self.y, 0, u, 16, w, 8, TRANSPARENT_COLOR)
 
-
-class Enemy1:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.dx = 0
-        self.dy = 0
-        self.direction = -1
-        self.is_alive = True
-
-    def update(self):
-        self.dx = self.direction
-        self.dy = min(self.dy + 1, 3)
-        if self.direction < 0 and is_wall(self.x - 1, self.y + 4):
-            self.direction = 1
-        elif self.direction > 0 and is_wall(self.x + 8, self.y + 4):
-            self.direction = -1
-        self.x, self.y, self.dx, self.dy = push_back(self.x, self.y, self.dx, self.dy)
-
-    def draw(self):
-        u = pyxel.frame_count // 4 % 2 * 8
-        w = 8 if self.direction > 0 else -8
-        pyxel.blt(self.x, self.y, 0, u, 24, w, 8, TRANSPARENT_COLOR)
-
-
-class Enemy2:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.dx = 0
-        self.dy = 0
-        self.direction = 1
-        self.is_alive = True
-
-    def update(self):
-        self.dx = self.direction
-        self.dy = min(self.dy + 1, 3)
-        if is_wall(self.x, self.y + 8) or is_wall(self.x + 7, self.y + 8):
-            if self.direction < 0 and (
-                is_wall(self.x - 1, self.y + 4) or not is_wall(self.x - 1, self.y + 8)
-            ):
-                self.direction = 1
-            elif self.direction > 0 and (
-                is_wall(self.x + 8, self.y + 4) or not is_wall(self.x + 7, self.y + 8)
-            ):
-                self.direction = -1
-        self.x, self.y, self.dx, self.dy = push_back(self.x, self.y, self.dx, self.dy)
-
-    def draw(self):
-        u = pyxel.frame_count // 4 % 2 * 8 + 16
-        w = 8 if self.direction > 0 else -8
-        pyxel.blt(self.x, self.y, 0, u, 24, w, 8, TRANSPARENT_COLOR)
-
-
-class Enemy3:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.time_to_fire = 0
-        self.is_alive = True
-
-    def update(self):
-        self.time_to_fire -= 1
-        if self.time_to_fire <= 0:
-            dx = player.x - self.x
-            dy = player.y - self.y
-            sq_dist = dx * dx + dy * dy
-            if sq_dist < 60**2:
-                dist = pyxel.sqrt(sq_dist)
-                enemies.append(Enemy3Bullet(self.x, self.y, dx / dist, dy / dist))
-                self.time_to_fire = 60
-
-    def draw(self):
-        u = pyxel.frame_count // 8 % 2 * 8
-        pyxel.blt(self.x, self.y, 0, u, 32, 8, 8, TRANSPARENT_COLOR)
-
-
-class Enemy3Bullet:
-    def __init__(self, x, y, dx, dy):
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
-        self.is_alive = True
-
-    def update(self):
-        self.x += self.dx
-        self.y += self.dy
-
-    def draw(self):
-        u = pyxel.frame_count // 2 % 2 * 8 + 16
-        pyxel.blt(self.x, self.y, 0, u, 32, 8, 8, TRANSPARENT_COLOR)
-
-
 class App:
     def __init__(self):
         pyxel.init(128, 128, title="Hibanana")
         pyxel.load("assets/textures.pyxres")
 
-        # Change enemy spawn tiles invisible
-        pyxel.image(0).rect(0, 8, 24, 8, TRANSPARENT_COLOR)
-
         global player
         player = Player(0, 0)
-        spawn_enemy(0, 127)
         pyxel.playm(0, loop=True)
         pyxel.run(self.update, self.draw)
 
@@ -245,14 +130,6 @@ class App:
             pyxel.quit()
 
         player.update()
-        for enemy in enemies:
-            if abs(player.x - enemy.x) < 6 and abs(player.y - enemy.y) < 6:
-                game_over()
-                return
-            enemy.update()
-            if enemy.x < scroll_x - 8 or enemy.x > scroll_x + 160 or enemy.y > 160:
-                enemy.is_alive = False
-        cleanup_list(enemies)
 
     def draw(self):
         pyxel.cls(0)
@@ -265,19 +142,14 @@ class App:
         # Draw characters
         pyxel.camera(scroll_x, 0)
         player.draw()
-        for enemy in enemies:
-            enemy.draw()
-
 
 def game_over():
-    global scroll_x, enemies
+    global scroll_x
     scroll_x = 0
     player.x = 0
     player.y = 0
     player.dx = 0
     player.dy = 0
-    enemies = []
-    spawn_enemy(0, 127)
     pyxel.play(3, 9)
 
 
